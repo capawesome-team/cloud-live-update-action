ea90a28990377e3e0b044d9e9b62005e2273c5bf
Robin Genz <mail@robingenz.dev>
2025-03-08 15:24:13 +0100

    wip

--- dist/index.mjs
+++ dist/index.mjs
@@ -2836,7 +2836,7 @@ const run = async () => {
   })
 
   // Install the CLI
-  await exec('npm install -g @capawesome/cli@1.4.0-dev.62a72e4.1741443606')
+  await exec('npm install -g @capawesome/cli@1.4.0-dev.62a72e4.1741443824')
   // Save the token
   await exec(`npx capawesome login --token ${token}`)
   // Create the channel
--- src/run.mjs
+++ src/run.mjs
@@ -14,7 +14,7 @@ export const run = async () => {
   })
 
   // Install the CLI
-  await exec('npm install -g @capawesome/cli@1.4.0-dev.62a72e4.1741443606')
+  await exec('npm install -g @capawesome/cli@1.4.0-dev.62a72e4.1741443824')
   // Save the token
   await exec(`npx capawesome login --token ${token}`)
   // Create the channel
