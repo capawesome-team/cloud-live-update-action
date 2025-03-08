623d3eb0265c04bccdb500c58070adca8127f7f6
Robin Genz <mail@robingenz.dev>
2025-03-08 15:41:13 +0100

    feat: enable git integration (#5)
    * feat: enable git integration
    
    * wip
    
    * fix
    
    * wip
    
    * wip
    
    * wip
    
    * revert

--- dist/index.mjs
+++ dist/index.mjs
@@ -2825,6 +2825,9 @@ const run = async () => {
     required: true
   })
   const channel = core.getInput('channel')
+  const commitMessage = core.getInput('commitMessage')
+  const commitRef = core.getInput('commitRef')
+  const commitSha = core.getInput('commitSha')
   const path = core.getInput('path', {
     required: true
   })
@@ -2833,14 +2836,16 @@ const run = async () => {
   })
 
   // Install the CLI
-  await exec('npm install -g @capawesome/cli@1.4.0')
+  await exec('npm install -g @capawesome/cli@1.6.0')
   // Save the token
   await exec(`npx capawesome login --token ${token}`)
   // Create the channel
   if (channel) {
     try {
       await exec(
-        `npx capawesome apps:channels:create --appId ${appId} --name ${channel}`
+        `npx capawesome apps:channels:create \
+          --appId ${appId} \
+          --name ${channel}`
       )
     } catch {
       // No-op
@@ -2848,7 +2853,13 @@ const run = async () => {
   }
   // Create the bundle
   const result = await exec(
-    `npx capawesome apps:bundles:create --appId ${appId} --channel ${channel} --path ${path}`
+    `npx capawesome apps:bundles:create \
+    --appId ${appId} \
+    --channel ${channel} \
+    --path ${path} \
+    ${commitMessage ? `--commit-message "${commitMessage}"` : ''} \
+    ${commitRef ? `--commit-ref ${commitRef}` : ''} \
+    ${commitSha ? `--commit-sha ${commitSha}` : ''}`
   )
   core.info(result)
 }
--- src/run.mjs
+++ src/run.mjs
@@ -6,6 +6,9 @@ export const run = async () => {
     required: true
   })
   const channel = core.getInput('channel')
+  const commitMessage = core.getInput('commitMessage')
+  const commitRef = core.getInput('commitRef')
+  const commitSha = core.getInput('commitSha')
   const path = core.getInput('path', {
     required: true
   })
@@ -14,14 +17,16 @@ export const run = async () => {
   })
 
   // Install the CLI
-  await exec('npm install -g @capawesome/cli@1.4.0')
+  await exec('npm install -g @capawesome/cli@1.6.0')
   // Save the token
   await exec(`npx capawesome login --token ${token}`)
   // Create the channel
   if (channel) {
     try {
       await exec(
-        `npx capawesome apps:channels:create --appId ${appId} --name ${channel}`
+        `npx capawesome apps:channels:create \
+          --appId ${appId} \
+          --name ${channel}`
       )
     } catch {
       // No-op
@@ -29,7 +34,13 @@ export const run = async () => {
   }
   // Create the bundle
   const result = await exec(
-    `npx capawesome apps:bundles:create --appId ${appId} --channel ${channel} --path ${path}`
+    `npx capawesome apps:bundles:create \
+    --appId ${appId} \
+    --channel ${channel} \
+    --path ${path} \
+    ${commitMessage ? `--commit-message "${commitMessage}"` : ''} \
+    ${commitRef ? `--commit-ref ${commitRef}` : ''} \
+    ${commitSha ? `--commit-sha ${commitSha}` : ''}`
   )
   core.info(result)
 }
